1931661856f9d3510e37ec136873911b4e02dd78
sammy.ha <sammy.ha@kingfisher.com>
2018-05-23 15:23:26 +0100

    shopping list html

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -2,5 +2,5 @@ export class Ingredient {
     // public name: string;
     // public amount: number;
 
-    constructor(public name: string, public amunt: number) {}
+    constructor(public name: string, public amount: number) {}
 }
