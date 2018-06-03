13d39e0a7c9c50c22b6e43be3672fe6b49cd3249
sammy.ha <sammy.ha@kingfisher.com>
2018-06-03 17:18:02 +0100

    added firebase  keys to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = '';
 
   onNavigate(feature) {
     this.loadedFeature = feature;
+  }
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAQi_QVoUoLlLbQSODkyDQetX1NFnJMw00',
+      authDomain: 'my-recipe-app-6e513.firebaseapp.com',
+    });
   }
 }
