0c8d4da42bdec84172d4af9fa1337370a8509364
sammy.ha <sammy.ha@kingfisher.com>
2018-06-04 11:27:08 +0100

    once logged in user is navigated to root page

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 import 'firebase/auth';
@@ -6,7 +7,7 @@ import 'firebase/auth';
 export class AuthService {
   token: string;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -19,6 +20,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then(
       response => {
+        this.router.navigate(['/']);
         firebase.auth().currentUser.getIdToken()
         .then(
           (token: string) => this.token = token
