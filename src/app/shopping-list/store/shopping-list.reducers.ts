166f15401296e53f55e27f3a78fff2eac9fc7232
sammy.ha <sammy.ha@kingfisher.com>
2018-06-06 17:38:51 +0100

    added update and delete action

--- src/app/shopping-list/store/shopping-list.reducers.ts
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -23,6 +23,25 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
                 ...state,
                 ingredients: [...state.ingredients, ...action.payload]
             };
+        case ShoppingListActions.UPDATE_INGREDIENT:
+            const ingredient = state.ingredients[action.payload.index];
+            const updatedIngredient = {
+                ...ingredient,
+                ...action.payload.ingredient
+            };
+            const ingredients = [...state.ingredients];
+            ingredients[action.payload.index] = updatedIngredient;
+            return {
+                ...state,
+                ingredients: ingredients
+            };
+        case ShoppingListActions.DELETE_INGREDIENT:
+            const oldIngredient = [...state.ingredients];
+            oldIngredient.splice(action.payload.index, 1);
+            return {
+                ...state,
+                ingredients: oldIngredient
+            };
         default:
         return state;
     }
