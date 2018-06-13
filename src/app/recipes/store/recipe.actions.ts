edf273acfeba0e9abce3540c6ab95a3d7915c6a3
sammy.ha <sammy.ha@kingfisher.com>
2018-06-13 12:04:20 +0100

    set out recipes actions

---
+++ src/app/recipes/store/recipe.actions.ts
@@ -0,0 +1,34 @@
+import { Recipe } from './../recipe.model';
+
+import { Action } from '@ngrx/store';
+
+export const SET_RECIPE = 'SET_RECIPE';
+export const ADD_RECIPE = 'ADD_RECIPE';
+export const UPDATE_RECIPE = 'UPDATE_RECIPE';
+export const DELETE_RECIPE = 'DELETE_RECIPE';
+
+export class SetRecipes implements Action {
+    readonly type = SET_RECIPE;
+
+    constructor(public payload: Recipe[]) {}
+}
+
+export class AddRecipes implements Action {
+    readonly type = ADD_RECIPE;
+
+    constructor(public payload: Recipe) {}
+}
+
+export class UpdateRecipes implements Action {
+    readonly type = UPDATE_RECIPE;
+
+    constructor(public payload: {index: number, updateRecipe: Recipe}) {}
+}
+
+export class DeleteRecipes implements Action {
+    readonly type = DELETE_RECIPE;
+
+    constructor(public payload: {index: number}) {}
+}
+
+export type RecipeActions = SetRecipes | AddRecipes | UpdateRecipes | DeleteRecipes;
